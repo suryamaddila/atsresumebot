ff61c48ee1677141b35ff9dd9718bcc3b4bcf5eb
suryamaddila <67618085+suryamaddila@users.noreply.github.com>
2025-08-07 12:32:08 +0530

    Delete handlers directory

--- handlers/start_handler.py
+++
@@ -1,5 +0,0 @@
-from telegram import Update
-from telegram.ext import ContextTypes
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Welcome! Please upload your resume as a PDF.")
